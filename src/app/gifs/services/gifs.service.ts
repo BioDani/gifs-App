b649a78ba3ba7420a13826aa8676d8f8ddc20b06
Daniel Tejada <dtejadah@unal.edu.co>
2022-08-23 10:39:13 -0500

    Se parametrizó la consulta a la API

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { SearchGifResponse, Gif } from '../interfaces/gifs.interfaces';
 
 @Injectable({
@@ -8,6 +8,7 @@ import { SearchGifResponse, Gif } from '../interfaces/gifs.interfaces';
 export class GifsService {
 
   private apiKey : string = "pfeexP4EH61Ixb7shkzfuLyWFt5p7bds" ;
+  private servicioURL: string = 'https://api.giphy.com/v1/gifs';
   private _historial: string[] = [];
 
 
@@ -18,22 +19,35 @@ export class GifsService {
     return [...this._historial];
   }
  
-  constructor( private http: HttpClient){
-
+  constructor( private http: HttpClient){ 
+    if (localStorage.getItem('historial')){
+      this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+      this.resultados = JSON.parse(localStorage.getItem('resultados')!) || [];
+    }
+    
   }
 
-  buscarGifs( query: string){
+  buscarGifs( query: string = ''){
 
     if (!(this._historial.includes(query))){
       this._historial.unshift(query);
       console.log(this._historial);
+
+      localStorage.setItem('historial', JSON.stringify(this._historial));
     }
 
-  this.http.get<SearchGifResponse>(`https://api.giphy.com/v1/gifs/search?api_key=pfeexP4EH61Ixb7shkzfuLyWFt5p7bds&q=${query}&limit =10`)
-   .subscribe((resp) => {
-    console.log(resp.data);
-    this.resultados = resp.data; 
-  });
+    const params = new HttpParams()
+    .set( 'api_key' , this.apiKey ) 
+    .set( 'q' , query )
+    .set( 'limit' , '10' );
+
+
+    this.http.get<SearchGifResponse>(`${ this.servicioURL }/search`,{ params})
+    .subscribe((resp) => {
+      console.log(resp.data);
+      this.resultados = resp.data; 
+      localStorage.setItem('resultados', JSON.stringify(this.resultados));
+    });
 
   }
 }
